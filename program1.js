b58f33c4c23ebdd082b38abfce9024b2426e6d73
Prakharkuls <prakharutk@gmail.com>
2024-04-25 13:55:42 +0530

    passed

--- program1.js
+++ program1.js
@@ -1,7 +1,26 @@
 function longestSubstring(s) {
-    // Implementation of longestSubstring function
+    let longest = 0;
+    let start = 0;
+    const charIndexMap = new Map();
+
+
+    for (let end = 0; end < s.length; end++) {
+        const currentChar = s[end];
+
+
+        if (charIndexMap.has(currentChar) && charIndexMap.get(currentChar) >= start) {
+
+            start = charIndexMap.get(currentChar) + 1;
+        }
+
+
+        charIndexMap.set(currentChar, end);
+
+
+        longest = Math.max(longest, end - start + 1);
+    }
+
+    return longest;
 }
 
 module.exports = { longestSubstring };
-
-
